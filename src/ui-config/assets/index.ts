a883097123105fa4af48a97a81312cc9709bca79
Onno Visser <visser.onno@gmail.com>
2021-08-31 14:33:03 +0200

    add markets and assets config

--- src/ui-config/assets/index.ts
+++ src/ui-config/assets/index.ts
@@ -1,3 +1,32 @@
-import { assetsList as assetsListFromKit, Asset } from '@aave/aave-ui-kit';
+import { Asset } from '@aave/aave-ui-kit';
 
-export const assetsList: Asset[] = assetsListFromKit;
+export const assetsList: Asset[] = [
+  {
+    name: 'DAI',
+    symbol: 'DAI',
+    color: '#f7b14a',
+    icon: 'https://storage.googleapis.com/tinlake/pool-media/dai.svg',
+    aIcon: 'https://storage.googleapis.com/tinlake/pool-media/adai.svg',
+  },
+  {
+    name: 'NS2DRP',
+    symbol: 'NS2DRP',
+    color: '#f7b14a',
+    icon: 'https://storage.googleapis.com/tinlake/pool-media/new-silver-2/drop.svg',
+    aIcon: 'https://storage.googleapis.com/tinlake/pool-media/new-silver-2/drop.svg',
+  },
+  {
+    name: 'CF4DRP',
+    symbol: 'CF4DRP',
+    color: '#f7b14a',
+    icon: 'https://storage.googleapis.com/tinlake/pool-media/consolfreight-4/drop.svg',
+    aIcon: 'https://storage.googleapis.com/tinlake/pool-media/consolfreight-4/drop.svg',
+  },
+  {
+    name: 'FF1DRP',
+    symbol: 'FF1DRP',
+    color: '#f7b14a',
+    icon: 'https://storage.googleapis.com/tinlake/pool-media/fortunafi/drop.svg',
+    aIcon: 'https://storage.googleapis.com/tinlake/pool-media/fortunafi/drop.svg',
+  },
+];
