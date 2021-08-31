2b8c0cf6f5ab1801e5c6b1f93b3da36c8c8f46e7
Onno Visser <visser.onno@gmail.com>
2021-08-31 15:25:41 +0200

    disable staking

--- src/ui-config/stake.ts
+++ src/ui-config/stake.ts
@@ -12,21 +12,7 @@ export interface StakeConfig {
   };
 }
 
-export const stakeConfig: StakeConfig | undefined = {
-  network: Network.mainnet,
-  stakeDataProvider: '0xa82247B44750ae23076D6746A9B5B8dc0eCBB646',
-  tokens: {
-    [Stake.aave]: {
-      TOKEN_STAKING: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
-      STAKING_REWARD_TOKEN: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
-      STAKING_HELPER: '0xce0424653fb2fd48ed1b621bdbd60db16b2e388a',
-    },
-    [Stake.bpt]: {
-      TOKEN_STAKING: '0xa1116930326D21fB917d5A27F1E9943A9595fb47',
-      STAKING_REWARD_TOKEN: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
-    },
-  },
-};
+export const stakeConfig: StakeConfig | undefined = undefined
 
 // kovan config
 // export const stakeConfig: StakeConfig | undefined = {
