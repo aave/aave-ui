dffcdf8987637e1f102d764469f466db1dec03cb
andyk <kyzia.ru@gmail.com>
2021-08-31 14:59:39 +0200

    log ipfs url after pinning

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,7 +3,7 @@ const cleanupAndPin = require('./helpers/pinata');
 
 async function pinAndPublish() {
   const hash = await cleanupAndPin();
-  console.log('Pinning was done successfully', hash);
+  console.log(`Pinning was done successfully: https://cloudflare-ipfs.com/ipfs/${hash}`);
 
   const domain = process.env.CF_DEPLOYMENT_DOMAIN;
   if (domain) {
