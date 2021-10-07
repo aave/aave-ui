1eb84eb75801ffebc2a2d3707361c2787f8882a7
Onno Visser <visser.onno@gmail.com>
2021-10-07 10:25:51 +0200

    add PERMISSION_MANAGER config

--- src/ui-config/markets/index.ts
+++ src/ui-config/markets/index.ts
@@ -23,6 +23,7 @@ export const marketsData: { [key in keyof typeof CustomMarket]: MarketDataType }
     addresses: {
       LENDING_POOL_ADDRESS_PROVIDER: '0xA9251111878BFA6b4cA83e6059e8C661d87b6ec2'.toLowerCase(),
       LENDING_POOL: '0xFa23272D6cD7Eb8413De7740D6b5017Ff93FD45e',
+      PERMISSION_MANAGER: '0x0',
     },
   },
 } as const;
