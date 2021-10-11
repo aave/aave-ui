187be032790a996000db230ce0edab8ff383526c
Andrew Schmidt <andrew@aave.com>
2021-10-11 00:06:07 -0500

    feat: display multiple incentive balances

--- src/components/TopIncentiveBalance/index.tsx
+++ src/components/TopIncentiveBalance/index.tsx
@@ -1,25 +1,51 @@
 import { useIntl } from 'react-intl';
 import { useThemeContext } from '@aave/aave-ui-kit';
 
-import { useDynamicPoolDataContext, useStaticPoolDataContext } from '../../libs/pool-data-provider';
+import { useDynamicPoolDataContext } from '../../libs/pool-data-provider';
 import Value from '../basic/Value';
-import Link from '../basic/Link';
-import DefaultButton from '../basic/DefaultButton';
 import { TokenIcon } from '../../helpers/markets/assets';
-
-import defaultMessages from '../../defaultMessages';
 import messages from './messages';
 import staticStyles from './style';
 import { useIncentivesDataContext } from '../../libs/pool-data-provider/hooks/use-incentives-data-context';
 import { normalize } from '@aave/math-utils';
+import { ComputedReserveData } from '@aave/protocol-js';
+
+import tribeIcon from '../../images/tirbe.svg';
+import { useState } from 'react';
+import IncentiveClaimDropdown from '../IncentiveClaimDropdown';
+
+// Fetch reward token symbol from hard coded non-reserve tokens or from reserves array
+export function getRewardTokenSymbol(
+  reserves: ComputedReserveData[],
+  rewardTokenAddress: string
+): string {
+  if (rewardTokenAddress.toLowerCase() === '0x4da27a545c0c5b758a6ba100e3a049001de870f5') {
+    return 'stkAAVE';
+  } else if (rewardTokenAddress.toLowerCase() === '0xc7283b66eb1eb5fb86327f08e1b5816b0720212b') {
+    return 'TRIBE';
+  } else if (rewardTokenAddress.toLowerCase() === '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7') {
+    return 'AVAX';
+  } else if (rewardTokenAddress.toLowerCase() === '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270') {
+    return 'MATIC';
+  } else {
+    let rewardReserve = reserves.find(
+      (reserve) => reserve.underlyingAsset.toLowerCase() === rewardTokenAddress.toLowerCase()
+    );
+    if (rewardReserve) {
+      return rewardReserve.symbol;
+    } else {
+      return '';
+    }
+  }
+}
 
 export default function TopIncentiveBalance() {
   const intl = useIntl();
   const { currentTheme, xl, sm } = useThemeContext();
 
   const { user, reserves } = useDynamicPoolDataContext();
-  const { userUnclaimedRewards } = useStaticPoolDataContext();
   const { userIncentives } = useIncentivesDataContext();
+  const [activeIncentiveHover, setActiveIncentiveHover] = useState<string>('');
 
   const hasEmission = reserves.find(
     (r) =>
@@ -28,16 +54,6 @@ export default function TopIncentiveBalance() {
 
   if (!user || !hasEmission) return null;
 
-  const totalRewards = Number(user.totalRewards) + Number(userUnclaimedRewards);
-
-  for (var key in userIncentives) {
-    console.log('NEW');
-    console.log(key);
-    console.log(normalize(userIncentives[key].claimableRewards, 18));
-  }
-
-  console.log('OLD');
-  console.log(totalRewards.toString());
   const iconSize = xl && !sm ? 14 : 18;
   return (
     <div className="TopIncentiveBalance">
@@ -45,36 +61,38 @@ export default function TopIncentiveBalance() {
 
       <div className="TopIncentiveBalance__right--inner">
         {Object.entries(userIncentives).map((incentive) => {
-          let rewardTokenSymbol = ''; // TO-DO: Create helper function or seperate way to fetch reward token symbol from address
-          if (incentive[1].rewardTokenAddress === '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5') {
-            rewardTokenSymbol = 'stkAAVE';
-          } else if (
-            incentive[1].rewardTokenAddress === '0x0000000000000000000000000000000000000000'
-          ) {
-            rewardTokenSymbol = 'DAI'; // this is just to test
-          }
+          const rewardTokenSymbol = getRewardTokenSymbol(reserves, incentive[1].rewardTokenAddress);
+          const claimableRewards = normalize(incentive[1].claimableRewards, 18);
           return (
-            <div className="TopIncentiveBalance__value--inner">
+            <div
+              className="TopIncentiveBalance__value--inner"
+              onMouseEnter={() => setActiveIncentiveHover(rewardTokenSymbol)}
+              onMouseLeave={() => setActiveIncentiveHover('')}
+            >
+              {rewardTokenSymbol === 'TRIBE' ? (
+                <img src={tribeIcon} alt="TRIBE icon" />
+              ) : (
+                <TokenIcon tokenSymbol={rewardTokenSymbol} height={iconSize} width={iconSize} />
+              )}
               <Value
-                value={normalize(incentive[1].claimableRewards, 18)}
+                value={claimableRewards}
                 maximumValueDecimals={2}
                 minimumValueDecimals={2}
                 color={!sm ? 'white' : 'dark'}
               />
-              <TokenIcon tokenSymbol={rewardTokenSymbol} height={iconSize} width={iconSize} />
+              <div
+                className="TopIncentiveBalance__hover-modal"
+                style={{ display: activeIncentiveHover === rewardTokenSymbol ? 'block' : 'none' }}
+              >
+                <IncentiveClaimDropdown
+                  symbol={rewardTokenSymbol}
+                  claimableRewards={claimableRewards}
+                  incentiveControllerAddress={incentive[0]}
+                />
+              </div>
             </div>
           );
         })}
-        {
-          // TO-DO: Instead of linking to claim page, call function on dashboard to display Your Rewards pop-up
-        }
-        <Link to="/rewards/confirm" className="ButtonLink" disabled={totalRewards === 0}>
-          <DefaultButton
-            title={intl.formatMessage(defaultMessages.claim)}
-            disabled={totalRewards === 0}
-            color="gradient"
-          />
-        </Link>
       </div>
 
       <style jsx={true} global={true}>
