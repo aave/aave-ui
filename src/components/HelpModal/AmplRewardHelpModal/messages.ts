dc075468f650f6bc605c946d59f7e53d0aa78e0e
Brandon Iles <brandon@fragments.org>
2021-10-22 09:30:21 -0700

    Update messages.ts

--- src/components/HelpModal/AmplRewardHelpModal/messages.ts
+++ src/components/HelpModal/AmplRewardHelpModal/messages.ts
@@ -3,10 +3,10 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   caption: 'AMPL token rewards',
   descriptionFirst:
-    'Ampleforth liquidity mining offers additional incentives for {ampl} depositors on AAVE.',
+    'Ampleforth liquidity mining offers additional benefits for {ampl} depositors on AAVE.',
   descriptionSecond:
-    'AAVE depositors can stake their aAMPL liquidity tokens on the {link} to receive a constant drip of $AMPL rewards from the reward pool.',
+    'AAVE depositors can stake their aAMPL liquidity tokens on the {link} to receive a stream of $AMPL rewards from the reward pool.',
   geyserApp: 'geyser application',
   descriptionThird:
-    'The Ampleforth geyser is a third party application NOT affiliated with AAVE in any way.',
+    'The Ampleforth geyser is a third party application not affiliated with AAVE in any way.',
 });
