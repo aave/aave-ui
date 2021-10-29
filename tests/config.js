281770e75f1b0ee9fe2d4c45223ee1eb65d34faf
NikitaQA <nikita@aave.com>
2021-10-29 10:37:10 +0100

    update tests to new reward UI

--- tests/config.js
+++ tests/config.js
@@ -1,5 +1,5 @@
 const { DOMAIN } = process.env;
 
 module.exports = {
-  URL: DOMAIN || 'https://staging.aave.com',
+  URL: DOMAIN || 'https://cloudflare-ipfs.com/ipfs/Qmciz3V16APyKZunpbVuMmFWKSXfLpuyq2iZ8LCfwsM5QC',
 };
