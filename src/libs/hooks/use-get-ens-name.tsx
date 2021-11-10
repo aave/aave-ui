513ede176a040c088d0719552a2cf12d9fc2009d
Andrew Schmidt <andrew@aave.com>
2021-11-10 16:37:35 -0600

    feat: add ens name hook

---
+++ src/libs/hooks/use-get-ens-name.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { Network } from '@aave/protocol-js';
+import { getProvider } from '../../helpers/markets/markets-data';
+
+const mainnetProvider = getProvider(Network.mainnet);
+
+const useGetEnsName = (address: string) => {
+  const [loading, setLoading] = useState(false);
+  const [ensName, setEnsName] = useState<string | undefined>(undefined);
+
+  const getRecord = async (address: string) => {
+    setLoading(true);
+    try {
+      const name = await mainnetProvider.lookupAddress(address);
+      setEnsName(name);
+    } catch (error) {
+      console.error('ENS lookup error', error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getRecord(address);
+  }, [address]);
+
+  return { ensName, loading };
+};
+
+export default useGetEnsName;
