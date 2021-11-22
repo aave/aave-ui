9c1a4b9c5eee3d514516b92ec3bdcff1770b211c
lukas strassel <lukasstrassel@googlemail.com>
2021-11-22 12:40:13 +0100

    fix: allow users to withdraw on frozen reserves

--- src/modules/deposit/components/DepositDashboardTable/DepositItem.tsx
+++ src/modules/deposit/components/DepositDashboardTable/DepositItem.tsx
@@ -79,7 +79,7 @@ export default function DepositItem({
         )}
 
         <TableButtonCol
-          disabled={!isActive || isFrozen}
+          disabled={!isActive}
           title={intl.formatMessage(defaultMessages.withdraw)}
           linkTo={`/withdraw/${underlyingAsset}-${id}`}
           withoutBorder={!isSwapButton}
