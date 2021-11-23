a089868fcdf738a23ff0b20c91ce712469408b3d
Jeroen Offerijns <jeroen@offerijns.com>
2021-11-23 12:27:30 +0100

    chore: update feed registry

--- src/ui-config/networks.ts
+++ src/ui-config/networks.ts
@@ -12,7 +12,7 @@ export const networkConfigs: Record<string, BaseNetworkConfig> = {
       walletBalanceProvider: '0x07DC923859b68e9399d787bf52c4Aa9eBe3490aF',
       uiPoolDataProvider: '0x6062ad399E47BF75AEa0b3c5BE7077c1E8664Dcb',
       uiIncentiveDataProvider: '0x9842E5B7b7C6cEDfB1952a388e050582Ff95645b',
-      chainlinkFeedRegistry: '0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0',
+      chainlinkFeedRegistry: '0x61DD0C4aFc62278E1a04db4201cd93eE67407fD5',
     },
     protocolDataUrl: 'https://api.thegraph.com/subgraphs/name/aave/protocol-v2-kovan',
     baseUniswapAdapter: '0xf86Be05f535EC2d217E4c6116B3fa147ee5C05A1',
