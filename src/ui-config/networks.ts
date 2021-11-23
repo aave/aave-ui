e2fd3a90f1ccff04db18f7d6c018df59f6b87233
Jeroen Offerijns <jeroen@offerijns.com>
2021-11-23 10:24:40 +0100

    fix: swap back ui pool data provider

--- src/ui-config/networks.ts
+++ src/ui-config/networks.ts
@@ -10,7 +10,7 @@ export const networkConfigs: Record<string, BaseNetworkConfig> = {
     publicJsonRPCUrl: ['https://kovan.poa.network'],
     addresses: {
       walletBalanceProvider: '0x07DC923859b68e9399d787bf52c4Aa9eBe3490aF',
-      uiPoolDataProvider: '0xD6BA0bDC0B437A891E9536AF367a70E21b6AFCF0',
+      uiPoolDataProvider: '0x6062ad399E47BF75AEa0b3c5BE7077c1E8664Dcb',
       uiIncentiveDataProvider: '0x9842E5B7b7C6cEDfB1952a388e050582Ff95645b',
       chainlinkFeedRegistry: '0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0',
     },
