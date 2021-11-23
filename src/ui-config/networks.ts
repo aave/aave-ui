b37b6363c2ffcf64eda46574978b530b05e21f76
lukas strassel <lukasstrassel@googlemail.com>
2021-11-23 13:51:04 +0100

    fix: add ethereum mainnet text

--- src/ui-config/networks.ts
+++ src/ui-config/networks.ts
@@ -28,7 +28,7 @@ export const networkConfigs: Record<string, BaseNetworkConfig> = {
     isTestnet: true,
   },
   [ChainId.mainnet]: {
-    name: 'Mainnet',
+    name: 'Ethereum mainnet',
     publicJsonRPCUrl: ['https://cloudflare-eth.com', 'https://eth-mainnet.alchemyapi.io/v2/demo'],
     publicJsonRPCWSUrl: 'wss://eth-mainnet.alchemyapi.io/v2/demo',
     addresses: {
