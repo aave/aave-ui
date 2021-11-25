dc273b0212eec826a06a69070d4b4db364e6bb4c
Andrew Schmidt <andrew@aave.com>
2021-11-25 00:49:46 -0600

    feat: add eMode enable/disable confirmation screen

---
+++ src/modules/emode/screens/EModeConfirm/messages.ts
@@ -0,0 +1,13 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  enableEmode: 'Enable E-Mode',
+  disableEmode: 'Disable E-Mode',
+  description:
+    'These are the transaction details. Make sure to check if this is correct before submitting.',
+  category: 'Category',
+  currentHealthFactor: 'Current Health Factor',
+  nextHealthFactor: 'Next Health Factor',
+  pleaseSubmitEnable: 'Please submit to enable E-mode',
+  pleaseSubmitDisable: 'Please submit to disable E-mode',
+});
