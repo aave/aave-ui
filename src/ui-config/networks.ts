531cbb6975cf8a56d0c012b099b3f1ee1092c847
sendra <polsendra@gmail.com>
2021-12-02 15:14:50 +0100

    feat: updated incentives provider

--- src/ui-config/networks.ts
+++ src/ui-config/networks.ts
@@ -59,7 +59,7 @@ export const networkConfigs: Record<string, BaseNetworkConfig> = {
     addresses: {
       walletBalanceProvider: '0x34aa032bC416Cf2CdC45c0C8f065b1F19463D43e',
       uiPoolDataProvider: '0xDb49B99073C2AE890708227Dc83B1781bF01512D',
-      uiIncentiveDataProvider: '0xC5093EDAC52f4DD68b42433eA8754B26eAbb1A48',
+      uiIncentiveDataProvider: '0x848482E84d358C0E23A40deB11Ca5b9c1984de81',
     },
     cachingServerUrl: 'https://cache-api-polygon.aave.com/graphql',
     cachingWSServerUrl: 'wss://cache-api-polygon.aave.com/graphql',
@@ -87,7 +87,7 @@ export const networkConfigs: Record<string, BaseNetworkConfig> = {
     addresses: {
       walletBalanceProvider: '0xEe7c0172c200e12AFEa3C34837052ec52F3f367A',
       uiPoolDataProvider: '0x9842E5B7b7C6cEDfB1952a388e050582Ff95645b',
-      uiIncentiveDataProvider: '0x04110Dc40B04b99B94840E53B2a33bE45E45A8Ed',
+      uiIncentiveDataProvider: '0x7eD30145eF0270Ebdfde2F39983C349C38Cb3ab4',
     },
     protocolDataUrl: 'https://api.thegraph.com/subgraphs/name/aave/aave-v2-polygon-mumbai',
     baseAsset: 'MATIC',
@@ -108,7 +108,7 @@ export const networkConfigs: Record<string, BaseNetworkConfig> = {
     addresses: {
       walletBalanceProvider: '0x3f5A507B33260a3869878B31FB90F04F451d28e3',
       uiPoolDataProvider: '0x93cc892330DFc071e87679468FeE054Cb05074ec',
-      uiIncentiveDataProvider: '0x2c911e6FaD423C00205eda22BBAc3e7F82c38007',
+      uiIncentiveDataProvider: '0xCdFbCB6bF78E57b640106CB4320Dde6cdf04e526',
     },
     protocolDataUrl: 'https://api.thegraph.com/subgraphs/name/aave/protocol-v2-fuji',
     baseUniswapAdapter: '0x0',
