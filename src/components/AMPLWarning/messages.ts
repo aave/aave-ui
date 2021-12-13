bfdb077e0453b95c181874129e53fba42a8d6b20
aalavandhan1984 <6264334+aalavandhan@users.noreply.github.com>
2021-12-13 10:25:28 -0500

    updated ampl faq message

--- src/components/AMPLWarning/messages.ts
+++ src/components/AMPLWarning/messages.ts
@@ -1,6 +1,8 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  amplWarning: '{asset} is an asset affected by rebasing. Visit the {link} to learn more.',
+  amplWarning:
+    '{asset} is an asset affected by rebasing. Visit the {link1} or {link2} to learn more.',
   documentation: 'documentation',
+  amplFaq: "Ampleforth's FAQ",
 });
