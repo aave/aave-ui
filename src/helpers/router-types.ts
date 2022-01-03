c0cd4b5f0cc6560abbcd3c1e17f02f78cf663936
lukas strassel <lukasstrassel@googlemail.com>
2022-01-03 12:03:13 +0100

    fix: stash current changes

--- src/helpers/router-types.ts
+++ src/helpers/router-types.ts
@@ -1,5 +1,1 @@
-export interface CurrencyRouteParamsInterface {
-  underlyingAsset: string;
-  id?: string;
-}
 export const CURRENCY_ROUTE_PARAMS = ':underlyingAsset([A-Z0-9x]+)(-?):id?';
--- src/modules/deposit/screens/DepositCurrency/index.tsx
+++ src/modules/deposit/screens/DepositCurrency/index.tsx
@@ -5,18 +5,14 @@ import { PERMISSION } from '@aave/contract-helpers';
 import DepositAmount from '../DepositAmount';
 import DepositConfirmation from '../DepositConfirmation';
 
-import { CURRENCY_ROUTE_PARAMS } from '../../../../helpers/router-types';
 import PermissionWarning from '../../../../ui-config/branding/PermissionWarning';
 
 export default function DepositCurrency() {
   return (
     <PermissionWarning requiredPermission={PERMISSION.DEPOSITOR}>
       <Routes>
-        <Route path={`/deposit/${CURRENCY_ROUTE_PARAMS}`} element={<DepositAmount />} />
-        <Route
-          path={`/deposit/${CURRENCY_ROUTE_PARAMS}/confirmation`}
-          element={<DepositConfirmation />}
-        />
+        <Route path={`/`} element={<DepositAmount />} />
+        <Route path={`confirmation`} element={<DepositConfirmation />} />
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </PermissionWarning>
