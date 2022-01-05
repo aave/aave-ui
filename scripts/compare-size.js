ac828a78a731263d5124267fa777c5b91219c6ef
Lukas <lukasstrassel@googlemail.com>
2022-01-05 12:16:23 +0100

    feat: track & comment bundle size change

---
+++ scripts/compare-size.js
@@ -0,0 +1,20 @@
+const stats = require('../stats.json');
+const masterStats = require('../stats_master.json');
+
+module.exports = async ({ github, context }) => {
+  if (github.context.eventName === 'pull_request') {
+    const masterMainChunk = masterStats.results.find((chunk) => /js\/main/.test(chunk.bundleName));
+    const masterMB = masterMainChunk.totalBytes / 1024 / 1024;
+
+    const currentMainChunk = stats.results.find((chunk) => /js\/main/.test(chunk.bundleName));
+    const currentMB = currentMainChunk.totalBytes / 1024 / 1024;
+
+    const diff = currentMB - masterMB;
+    await github.rest.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: `Bundle size change: ${diff}MB`,
+    });
+  }
+};
