e06bc23a591077e589d3477a7786e1a93665dda8
Prithpal Sooriya <prithpal.sooriya@gmail.com>
2022-01-06 17:17:36 +0000

    Feat - add aave version query param

--- src/components/HelpModal/HealthFactorHelpModal/index.tsx
+++ src/components/HelpModal/HealthFactorHelpModal/index.tsx
@@ -10,6 +10,31 @@ import bellGray from './images/bellGray.svg';
 import bellGrayDark from './images/bellGrayDark.svg';
 import { AdditionalItemProps } from '../../TextWithModal';
 import { useUserWalletDataContext } from '../../../libs/web3-data-provider';
+import { useProtocolDataContext } from '../../../libs/protocol-data-provider';
+import { CustomMarket } from '../../../ui-config/markets';
+
+const marketToHALAaveVersionUrlParam = (market: CustomMarket): string | undefined => {
+  const exhaustCases = (_: never) => undefined;
+  switch (market) {
+    case CustomMarket.proto_matic:
+      return 'aavepolygon';
+    case CustomMarket.proto_avalanche:
+      return 'aaveavalanche';
+    case CustomMarket.proto_mainnet:
+      return 'aavev2';
+
+    // unhandled casses
+    case CustomMarket.proto_kovan:
+    case CustomMarket.proto_mumbai:
+    case CustomMarket.proto_fuji:
+    case CustomMarket.amm_kovan:
+    case CustomMarket.amm_mainnet:
+      return undefined;
+
+    default:
+      return exhaustCases(market);
+  }
+};
 
 const HALNotificationIcon: React.FC<AdditionalItemProps> = ({
   height,
@@ -19,13 +44,19 @@ const HALNotificationIcon: React.FC<AdditionalItemProps> = ({
   iconTheme,
 }) => {
   const { currentAccount } = useUserWalletDataContext();
+  const { currentMarket } = useProtocolDataContext();
 
   const urlString = React.useMemo(() => {
     const url = new URL('https://9000.hal.xyz/recipes/aave-track-your-health-factor');
     url.searchParams.set('user', currentAccount);
 
+    const aaveVersionParam = marketToHALAaveVersionUrlParam(currentMarket);
+    if (aaveVersionParam) {
+      url.searchParams.set('aaveversion', aaveVersionParam);
+    }
+
     return url.toString();
-  }, [currentAccount]);
+  }, [currentAccount, currentMarket]);
 
   return (
     <a
