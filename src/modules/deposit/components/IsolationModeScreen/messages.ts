fd1b16f0eedde3ac43164e0719171b4d670bd2df
Mark Hinschberger <markhinschberger@gmail.com>
2022-01-07 14:24:31 +0100

    update isolation mode text

--- src/modules/deposit/components/IsolationModeScreen/messages.ts
+++ src/modules/deposit/components/IsolationModeScreen/messages.ts
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   caption: 'You are entering isolation mode',
   description:
-    'In isolation mode you cannot supply other assets as collateral for borrowing. Assets used as collateral in isolation mode can only be borrowed to a specific debt ceiling.',
+    'In isolation mode you cannot supply other assets as collateral for borrowing. Assets used as collateral in isolation mode can only be borrowed to a specific debt ceiling. You can only borrow certain assets in isolation mode.',
 
   learnMore: 'Learn more in our {link}.',
   FAQGuide: 'FAQ guide',
