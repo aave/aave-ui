5d486d052de4523f9870e95de5aa12a020daa16a
Lukas <lukasstrassel@googlemail.com>
2022-01-07 18:04:41 +0100

    fix: store hash for every build (#327)

--- scripts/compare-size.js
+++ scripts/compare-size.js
@@ -1,10 +1,10 @@
 const stats = require('../stats.json');
-const masterStats = require('../stats_master.json');
+const headStats = require('../stats_head.json');
 
 /**
  * Whenever a pr is merged to master the ci will take a snapshot of the sourcemaps generated.
  * When a pr is created or a commit is pushed the ci will:
- * 1. download that snapshot `stats_master.json`
+ * 1. download that snapshot `stats_head.json`
  * 2. create a new snapshot based on current build `stats.json`
  * Try to do a reasonable comparison.
  *
@@ -15,7 +15,7 @@ const masterStats = require('../stats_master.json');
  */
 module.exports = async ({ github, context }) => {
   if (context.eventName === 'pull_request') {
-    const masterMainChunk = masterStats.results.find((chunk) => /js\/main/.test(chunk.bundleName));
+    const masterMainChunk = headStats.results.find((chunk) => /js\/main/.test(chunk.bundleName));
     const masterMB = masterMainChunk.totalBytes / 1024 / 1024;
 
     const currentMainChunk = stats.results.find((chunk) => /js\/main/.test(chunk.bundleName));
