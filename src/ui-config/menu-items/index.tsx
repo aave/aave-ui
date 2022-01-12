840f89378640ac3209e5d56860cd899046a2bc85
Pol Sendra <sendra@users.noreply.github.com>
2022-01-12 16:34:08 +0100

    fix: remove faucet (#345)

--- src/ui-config/menu-items/index.tsx
+++ src/ui-config/menu-items/index.tsx
@@ -1,13 +1,7 @@
 import { Navigation } from '../../components/menu/navigation';
 import messages from './messages';
-import { isFeatureEnabled } from '../../helpers/config/markets-and-network-config';
 
 export const moreMenuItems: Navigation[] = [
-  {
-    link: '/faucet',
-    title: messages.faucet,
-    isVisible: isFeatureEnabled.faucet,
-  },
   {
     link: 'https://docs.aave.com/faq/',
     title: messages.faq,
