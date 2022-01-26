3f28ba40a54d056a76507eeb8653692789023b13
sendra <polsendra@gmail.com>
2022-01-26 11:38:20 +0100

    feat: added eth icon and commented V2 markets

--- src/ui-config/markets/images/index.tsx
+++ src/ui-config/markets/images/index.tsx
@@ -1,6 +1,7 @@
 import aaveLogo from './aave.svg';
 import aaveActiveLogo from './aaveActive.svg';
 import aavev2Logo from './aavev2.svg';
+import aaveV3Logo from './aaveV3.svg';
 import aavev2ActiveLogo from './aavev2Active.svg';
 import ammLogo from './amm.svg';
 import ammActiveLogo from './ammActive.svg';
@@ -10,6 +11,7 @@ import avalanche from './avalanche.svg';
 import optimis from './optimis.svg';
 import harmony from './harmony.svg';
 import fantom from './fantom.svg';
+import ethereum from './ethereum.svg';
 
 export {
   aaveLogo,
@@ -24,4 +26,6 @@ export {
   optimis,
   harmony,
   fantom,
+  aaveV3Logo,
+  ethereum,
 };
